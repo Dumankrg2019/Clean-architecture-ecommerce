1087318b41af4f516d5b1fe289f12b48acc24b0d
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2023-11-21 22:17:51 +0600

    feat: first build

---
+++ app/src/main/java/com/example/ecommerceproject/ProductListViewState.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerceproject
+
+sealed class ProductListViewState {
+    object Loading : ProductListViewState()
+    data class Content(val productList: List<ProductCardViewState>) : ProductListViewState()
+    object Error : ProductListViewState()
+}
